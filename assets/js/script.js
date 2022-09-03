03cf2c519b28da8a281c525f58f8e083692348ad
John_considine <johnconsidine23@yahoo.co.uk>
2022-09-03 16:07:16 +0000

    add const variables to javaScript file and place inside of a function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,44 @@
-console.log("hello world")
+$(document).ready(function () {
+
+    const userScoretext = $("#userscore");
+    const botScoretext = $("#botscore");
+    const player = $("#player");
+    const bot = $("#bot");
+    const rock = $("#rock");
+    const paper = $("#paper");
+    const scissors = $("#scissors");
+    const lizard = $("#lizard");
+    const spock = $("#spock");
+
+    let userScore = 0;
+    let botScore = 0;
+
+    botScoretext.text("897");
+
+    paper.click(function () {
+        userScoretext.text("10");
+    })
+
+
+    rock.click(function () {
+        alert("test")
+
+
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});

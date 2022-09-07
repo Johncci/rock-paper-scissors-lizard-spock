289a78dd8ae647cb4b7973c009eaf92f30cfe7d2
John_considine <johnconsidine23@yahoo.co.uk>
2022-09-07 20:45:28 +0000

    add js fucntionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,39 +10,63 @@ $(document).ready(function () {
     const lizard = $("#lizard");
     const spock = $("#spock");
 
+    const options = {
+        ROCK: 0,
+        PAPER: 1,
+        SCISSORS: 2,
+        LIZARD: 3,
+        SPOCK: 4,
+    }
+
     let userScore = 0;
     let botScore = 0;
 
     botScoretext.text("0");
 
     paper.click(function () {
-        userScoretext.text("99");
-    })
-
-    spock.click(function () {
-        botScoretext.text("45");
-    })
-
-
-    rock.click(function () {
-        alert("test")
-
-
+        playBotTurn();
     });
 
+    scissors.click(function () {
+        playBotTurn();
+    });
 
+    rock.click(function () {
+        playBotTurn();
+    });
 
+    lizard.click(function () {
+        playBotTurn();
+    });
 
+    spock.click(function () {
+        playBotTurn();
+    });
 
+    function playBotTurn() {
+        let random = Math.floor(Math.random() * 5);
+        let randomOptionImage = getOptionImage(random);
+        bot.attr("id", randomOptionImage);
 
+    }
 
+    function getOptionImage(option) {
+        switch (option) {
+            case options.ROCK:
+                return "assets/images/rock.icon.jpg";
+            case options.PAPER:
+                return "assets/images/paper.icon.jpg";
+            case options.SCISSORS:
+                return "assets/images/scissors.icon.jpg";
+            case options.LIZARD:
+                return "assets/images/lizard.icon.jpg";
+            case options.SPOCK:
+                return "assets/images/spock..jpg";
+            default:
+                return "";
+        }
 
-
-
-
-
-
-
+    }
 
 
 });
